5c62df43dfa334c139b4dcab21d33666dc60f4c3
liutangqi <liutangqi@quanqiuwa.com>
2020-04-08 17:53:42 +0800

    去除多余的信息

--- src/main/java/com/sangsang/jdncsharding/JdncshardingApplication.java
+++ src/main/java/com/sangsang/jdncsharding/JdncshardingApplication.java
@@ -1,18 +1,15 @@
 package com.sangsang.jdncsharding;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //排除DataSourceConfiguratrion
+@SpringBootConfiguration
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})//排除DataSourceConfiguratrion
 @EnableTransactionManagement(proxyTargetClass = true)   //开启事物管理功能
 public class JdncshardingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JdncshardingApplication.class, args);
     }
-
 }
--- src/main/java/com/sangsang/jdncsharding/domain/UserEntity.java
+++ src/main/java/com/sangsang/jdncsharding/domain/UserEntity.java
@@ -1,13 +1,8 @@
 package com.sangsang.jdncsharding.domain;
-
 import com.sangsang.jdncsharding.enums.UserSexEnum;
 import lombok.Data;
-
-import java.io.Serializable;
-
 @Data
-public class UserEntity implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class UserEntity  {
     private Long id;
     private Long order_id;
     private Long user_id;
